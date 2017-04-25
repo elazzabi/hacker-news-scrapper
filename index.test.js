550e977774c66fdb66ac9e9ea06bfa805bbfc1e3
Ahmed <elazzabi.ahmed@gmail.com>
2017-04-25 22:09:54 +0100

    Refactor tests

--- index.test.js
+++ index.test.js
@@ -1,18 +1,14 @@
 const page = require('./index').page
 const test = require('ava')
 
-test("Expect 'page' to work without arguments", t => {
-    let callback = (data) => {
-        t.is(data.length, 30)
-    }
-    
-    return page().then(callback)
+let callback = (t, data) => {
+    t.is(data.length, 30)
+}
+
+test("Expect 'page' to work without arguments", t => {    
+    return page().then(callback.bind(null, t))
 })
 
-test("Expect 'page' to work with argument", t => {
-    let callback = (data) => {
-        t.is(data.length, 30)
-    }
-    
-    return page(2).then(callback)
+test("Expect 'page' to work with arguments", t => {
+    return page(2).then(callback.bind(null, t))
 })
